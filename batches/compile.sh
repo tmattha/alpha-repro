d2281258f29e0ec95d39930a5502ab7d5093c41a
Tilmann Matthaei <tilmann.matthaei@uni-wuppertal.de>
2023-06-23 10:54:07 +0000

    slightly adjust compile job

--- batches/compile.sh
+++ batches/compile.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 #SBATCH --account=hwu29
-# budget account where contingent is taken from
 #SBATCH --nodes=1
-#SBATCH --ntasks-per-node=1
-# if keyword omitted: Max. 96 tasks per node
-# (SMT enabled, see comment below)
-#SBATCH --cpus-per-task=40
-# for OpenMP/hybrid jobs only
 #SBATCH --time=00:10:00
 #SBATCH --partition=develgpus
 
